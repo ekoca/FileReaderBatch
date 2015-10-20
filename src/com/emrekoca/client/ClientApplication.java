c93074de1c00392d540b2220940740de52439d67
Emre <ekocausa@gmail.com>
2015-10-19 23:19:18 -0700

    Application context shutdown

--- src/com/emrekoca/client/ClientApplication.java
+++ src/com/emrekoca/client/ClientApplication.java
@@ -5,12 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.emrekoca.motd.MessageOfTheDayService;
 
 public class ClientApplication {
+	private static ClassPathXmlApplicationContext container;
+
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
+		container = new ClassPathXmlApplicationContext("application.xml");
 		MessageOfTheDayService helloWorld = 
 				container.getBean("motdService", MessageOfTheDayService.class);
 		System.out.println(helloWorld.getTodaysMessage());
 		// Spring clean up resources!
-		container.close();
+		container.registerShutdownHook();
 	}
 }
