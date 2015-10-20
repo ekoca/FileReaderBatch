5994c9672a7846b0d012cb7e89eadfd817599a49
Emre <ekocausa@gmail.com>
2015-10-19 23:57:49 -0700

    Autowired functionality

--- src/com/emrekoca/autowired/Hello.java
+++ src/com/emrekoca/autowired/Hello.java
@@ -1,11 +1,14 @@
 package com.emrekoca.autowired;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Hello {
+	static Logger logger = Logger.getLogger(Hello.class);
 	@Override
 	public String toString() {
+		logger.info("Hello log :");
 		return "Hello world";
 	}
 }
