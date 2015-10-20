7305f407213006c1921f99a37aad456c549105ab
Emre <ekocausa@gmail.com>
2015-10-19 23:46:20 -0700

    Autowired functionality

---
+++ src/com/emrekoca/autowired/Hello.java
@@ -0,0 +1,11 @@
+package com.emrekoca.autowired;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+	@Override
+	public String toString() {
+		return "Hello world";
+	}
+}
--- src/com/emrekoca/client/ClientApplication.java
+++ src/com/emrekoca/client/ClientApplication.java
@@ -2,16 +2,15 @@ package com.emrekoca.client;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.emrekoca.motd.MessageOfTheDayService;
+import com.emrekoca.autowired.Hello;
 
 public class ClientApplication {
 	private static ClassPathXmlApplicationContext container;
 
 	public static void main(String[] args) {
 		container = new ClassPathXmlApplicationContext("application.xml");
-		MessageOfTheDayService helloWorld = 
-				container.getBean("motdService", MessageOfTheDayService.class);
-		System.out.println(helloWorld.getTodaysMessage());
+		Hello helloWorld = container.getBean("hello", Hello.class);
+		System.out.println(helloWorld);
 		// Spring clean up resources!
 		container.registerShutdownHook();
 	}
